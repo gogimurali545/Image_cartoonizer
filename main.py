e506175c36ae6eb25c3f326ec0d352aaeba60032
gogimurali545 <muralidharreddy545@hotmail.com>
2021-12-14 22:57:22 -0500

    almost final code

--- main.py
+++ main.py
@@ -7,31 +7,66 @@ import base64
 from io import BytesIO
 import os
 
-uploaded_file = st.file_uploader("Upload an image", type=['png', 'jpeg'])
+st.set_page_config(page_title="Pirates of Python", page_icon="https://cdn-icons-png.flaticon.com/128/71/71270.png")
+
+st.title("Image Cartoonizer")
+
+def cartoonify(image):
+    gray_Image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    blur_Gray_Image = cv2.medianBlur(gray_Image, 5)
+    edge_Image = cv2.adaptiveThreshold(blur_Gray_Image, 255, cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY, 7, 7)
+    color_Image = cv2.bilateralFilter(image, 9, 300, 300)
+    cartoonified_Image = cv2.bitwise_and(color_Image, color_Image, mask=edge_Image)
+    return gray_Image, blur_Gray_Image, edge_Image, color_Image, cartoonified_Image
+
+def buffer_data(image):
+    image_from_array = Image.fromarray(image)
+    buf = BytesIO()
+    image_from_array.save(buf, format='JPEG')
+    return image_from_array, buf.getvalue()
+
+uploaded_file = st.file_uploader("Upload an image", type=['png', 'jpeg', 'jpg'])
 if uploaded_file is not None:
     image_original = np.asarray(Image.open(uploaded_file))
-    # var =str(image_original.shape)
-    # st.text(var)
-    # image_gray = cv2.cvtColor(image_original, cv2.COLOR_BGR2GRAY)
-    # image_gray = cv2.resize(image_gray, (960, 540))
-    # st.image(image_gray)
-    # var1 = str(image_gray.shape)
-    # st.text(var1)
-    def cartoonify(image):
-        grayScaleImage = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-        # ReSized2 = cv2.resize(grayScaleImage, (450, 540))
-
-        return grayScaleImage
-
-    final_image = cartoonify(image_original)
-    st.image(final_image)
-    final_image = Image.fromarray(final_image)
-    buf = BytesIO()
-    final_image.save(buf, format='JPEG')
-    # img = plt.imshow(final_image)
-    
-    st.download_button(label="Download gray image", data=buf.getvalue(), mime="image/png")
-
-    
     
 
+    [image_2, image_3, image_4, image_5, Cartoonified_Image] = cartoonify(image_original)
+
+    col1, col2 = st.columns(2)
+
+
+    original_image, original_data = buffer_data(image_original)
+    image_2, image_2_data = buffer_data(image_2)
+    image_3, image_3_data = buffer_data(image_3)
+    image_4, image_4_data = buffer_data(image_4)
+    image_5, image_5_data = buffer_data(image_5)
+    cartoonified_image , cartoonified_data = buffer_data(Cartoonified_Image)
+    
+    col1.header('Original Image')
+    col1.image(original_image)
+    col1.download_button(label="Download Original Image", data=original_data, mime="image/png")
+
+    col2.header('Gray Image')
+    col2.image(image_2)
+    col2.download_button(label="Download gray image", data=image_2_data, mime="image/png")
+
+    col1.header('Image 3')
+    col1.image(image_3)
+    col1.download_button(label="Download Image 3", data=image_3_data, mime="image/png")
+
+    col2.header('Image 4')
+    col2.image(image_4)
+    col2.download_button(label="Download Image 4", data=image_4_data, mime="image/png")
+
+    col1.header('Image 5')
+    col1.image(image_5)
+    col1.download_button(label="Download Image 5", data=image_5_data, mime="image/png")
+
+    col2.header('Cartoonified Image')
+    col2.image(cartoonified_image)
+    col2.download_button(label="Download Cartoonified Image", data=cartoonified_data, mime="image/png")
+
+    st.header('Project Done by: ')
+    st.write('Muralidhar Reddy 216')
+    st.write('Seetharam Dasari 481')
+    st.write('Suraj Alamoni 558')
